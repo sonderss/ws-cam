97955db8b87b5fc08f469b4fbbfecee5b06b2493
sonders <778277514@qq.com>
2022-07-26 18:41:44 +0800

    ws模块

---
+++ src/events.gateway.ts
@@ -0,0 +1,25 @@
+import {
+  SubscribeMessage,
+  WebSocketGateway,
+  MessageBody,
+  ConnectedSocket,
+} from '@nestjs/websockets';
+
+import * as WebSocket from 'ws';
+@WebSocketGateway(3021)
+export class EventsGateway {
+  @SubscribeMessage('message')
+  handleMessage(client: any, payload: any): string {
+    console.log('client', client);
+    console.log('payload', payload);
+    return 'Hello world!';
+  }
+  @SubscribeMessage('hello2')
+  hello2(@MessageBody() data: any, @ConnectedSocket() client: WebSocket): any {
+    console.log('收到消息 client:', client);
+
+    client.send(JSON.stringify({ event: 'tmp', data: '这里是个临时信息' }));
+
+    return { event: 'hello2', data: data };
+  }
+}
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,9 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-
+import { WsAdapter } from './ws.adapter';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useWebSocketAdapter(new WsAdapter(app)); // 使用我们的适配器
   await app.listen(3020);
 }
 bootstrap();
